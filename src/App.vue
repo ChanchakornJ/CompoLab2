a3f6cfa1e255962a7706b7b853df4664b2b8d773
ChanchakornJ <chanchakornj@gmail.com>
2026-07-12 16:58:32 +0700

    7 cat adoption event card

--- src/App.vue
+++ src/App.vue
@@ -12,26 +12,26 @@ import { RouterLink, RouterView } from 'vue-router'
         </nav>
       </div>
     </header>
+
+    <RouterView />
   </div>
 </template>
 
 <style scoped>
-
- #layout {
+#layout {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
- }
-
+}
 
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active{
+nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
