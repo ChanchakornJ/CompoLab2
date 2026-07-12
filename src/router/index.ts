f055fbb88c32d42cf259394871839e2b44734381
ChanchakornJ <chanchakornj@gmail.com>
2026-07-12 19:01:15 +0700

    16.3 new student service

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
+import StudentListView from '@/views/StudentListView.vue'
 import AboutView from '@/views/AboutView.vue'
 
 const router = createRouter({
@@ -9,12 +10,18 @@ const router = createRouter({
       path: '/',
       name: 'event-list-view',
       component: EventListView,
+      
     },
     {
       path: '/about',
       name: 'about',
      component: AboutView
     },
+    {
+      path: '/student',
+      name: 'student-list-view',
+     component: StudentListView
+    }
   ],
 })
 
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -3,14 +3,15 @@ import EventCard from '@/components/EventCard.vue'
 import EventCardAlt from '@/components/EventCardAlt.vue'
 import type { Event } from '@/types'
 import {ref, onMounted} from 'vue'
-import axios from 'axios'
+import EventService from '@/services/EventService'
+
 const events = ref<Event[] | null>(null)
 
 onMounted(() =>{
-  axios
-    .get('https://my-json-server.typicode.com/ChanchakornJ/compo-json/events')
+   EventService.getEvents()
     .then((response) =>{
       console.log(response.data)
+      events.value = response.data
     })
     .catch((error) => {
       console.error('There was an error!', error)
