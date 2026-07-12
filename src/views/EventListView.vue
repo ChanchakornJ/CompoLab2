35a346d68ad9201ac3eb6ca31fad3a33aed879fa
ChanchakornJ <chanchakornj@gmail.com>
2026-07-12 18:28:24 +0700

    15.3 data in console

---
+++ src/views/EventListView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import EventCardAlt from '@/components/EventCardAlt.vue'
+import type { Event } from '@/types'
+import {ref, onMounted} from 'vue'
+import axios from 'axios'
+const events = ref<Event[] | null>(null)
+
+onMounted(() =>{
+  axios
+    .get('https://my-json-server.typicode.com/ChanchakornJ/compo-json/events')
+    .then((response) =>{
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+</script>
+
+<template>
+      <h1>Events For Good</h1>
+
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <EventCardAlt v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
