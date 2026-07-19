faa2c896ce7fd3c2ca8a63d991eaf321b3c73bff
ChanchakornJ <chanchakornj@gmail.com>
2026-07-19 23:17:56 +0700

    1.9 router link used components' name

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import StudentListView from '@/views/StudentListView.vue'
 import AboutView from '@/views/AboutView.vue'
+import EventDetailView from '@/views/EventDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,8 +10,14 @@ const router = createRouter({
     {
       path: '/',
       name: 'event-list-view',
-      component: EventListView,
+      component: EventListView
       
+    },
+    {
+      path: '/event/:id',
+      name: 'event-detail-view',
+      component: EventDetailView,
+      props: true
     },
     {
       path: '/about',
--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -12,5 +12,8 @@ const apiClient = axios.create({
 export default {
     getEvents(){
         return apiClient.get('/events')
+    },
+    getEvent(id: number){
+        return axios.get('/events/' + id)
     }
 }
