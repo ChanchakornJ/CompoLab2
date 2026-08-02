4c7b52e4115aceea2825ba3a97734fb16854103d
ChanchakornJ <chanchakornj@gmail.com>
2026-08-03 02:18:41 +0700

    2.3 all menu progress bar

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@ import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/event/NetworkErrorView.vue'
+import nProgress from 'nprogress'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -72,6 +73,13 @@ const router = createRouter({
      component: StudentListView
     }
   ],
+})
+router.beforeEach(() =>{
+  nProgress.start()
+})
+
+router.afterEach(() =>{
+  nProgress.done()
 })
 
 export default router
