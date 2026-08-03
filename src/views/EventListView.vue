478b3d776b7003c5fd7e72ebbfc76f463e5d9b27
ChanchakornJ <Chanchakornj@gmail.com>
2026-08-03 13:18:09 +0700

    2.2 replace event with tailwind

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -44,7 +44,7 @@ onMounted(() =>{
 <template>
       <h1>Events For Good</h1>
 
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
     <!-- <EventCardAlt v-for="event in events" :key="event.id" :event="event"/> -->
   <div class="pagination">
@@ -68,11 +68,7 @@ onMounted(() =>{
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+
 .pagination {
 display: flex;
 width: 290px;
